efabd0123ff8fbeff14a3f67daaa395272a5b6e4
getpro <flowerdo@126.com>
2017-06-09 00:49:22 +0800

    测试中文

--- src/main/java/com/example/IndexController.java
+++ src/main/java/com/example/IndexController.java
@@ -7,7 +7,9 @@ import org.nutz.plugin.spring.boot.config.SqlManagerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import win.i02.bean.ElementBean;
 
@@ -38,11 +40,25 @@ public class IndexController {
     }
 
     @RequestMapping("/add")
-    public String add(){
+    public String add(String cname,String ename){
         ElementBean bean = new ElementBean();
-        bean.setcNameCn("测试");
-        bean.setcNameEn("test");
+        bean.setcNameCn(cname);
+        bean.setcNameEn(ename);
         dao.insert(bean);
         return "ok";
+    }
+
+    @RequestMapping("/del/{id}")
+    public String del(@PathVariable int id){
+        int result = dao.delete(ElementBean.class,id);
+        if(result>0){
+            return "ok";
+        }
+        return "fail";
+    }
+
+    @RequestMapping("/cn")
+    public String cn(){
+        return "中文";
     }
 }
